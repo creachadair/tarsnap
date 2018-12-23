0088b5e40a00aee041a3281b17a1a28251715257
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-12-22 17:06:14 -0800

    Add a Stats function to gather archive sizes.

--- tarsnap.go
+++ tarsnap.go
@@ -8,7 +8,9 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -23,6 +25,9 @@ func Create(name string, opts CreateOptions) error { return std.Create(name, opt
 
 // Delete deletes an archive in the default config.
 func Delete(archives ...string) error { return std.Delete(archives...) }
+
+// Stats reports archive size statistics in the default config.
+func Stats(archives ...string) (*SizeInfo, error) { return std.Stats(archives...) }
 
 // Config carries configuration settings to a tarsnap execution.  A nil *Config
 // is ready for use and provides default settings.
@@ -135,6 +140,89 @@ func (c *Config) Delete(archives ...string) error {
 		args = append(args, "-f", a)
 	}
 	return c.run(args)
+}
+
+// Stats reports storage statistics for the specified archives.  If no archives
+// are specified only global stats are reported.
+func (c *Config) Stats(archives ...string) (*SizeInfo, error) {
+	args := []string{"--print-stats", "--no-humanize-numbers"}
+	for _, arch := range archives {
+		args = append(args, "-f", arch)
+	}
+	return maybeParseSizeInfo(c.runOutput(args))
+}
+
+// Sizes represents storage size values.
+type Sizes struct {
+	InputBytes            int64 // total bytes of original input
+	CompressedBytes       int64 // size after input compression
+	UniqueBytes           int64 // size after deduplication
+	CompressedUniqueBytes int64 // size after deduplication and compression
+}
+
+func (s *Sizes) String() string {
+	return fmt.Sprintf("#<size: input=%d, compressed=%d, unique=%d, cunique=%d>",
+		s.InputBytes, s.CompressedBytes, s.UniqueBytes, s.CompressedUniqueBytes)
+}
+
+// SizeInfo records storage size information for archives.
+type SizeInfo struct {
+	All     *Sizes            // sizes for all archives known
+	Archive map[string]*Sizes // sizes for individual archives
+}
+
+var sizes = regexp.MustCompile(`^\s*(.*?)\s+(\d+)\s+(\d+)$`)
+
+func maybeParseSizeInfo(data []byte, err error) (*SizeInfo, error) {
+	if err != nil {
+		return nil, err
+	}
+	info := &SizeInfo{Archive: make(map[string]*Sizes)}
+	var cur *Sizes
+	for i, line := range strings.Split(string(data), "\n") {
+		m := sizes.FindStringSubmatch(line)
+		if m == nil {
+			continue // skip header row
+		}
+
+		// Syntax:  <name>    <total-size> <compressed-size>
+		//
+		// The name may contain spaces, so the regexp specifically globs
+		// everything up to the final two numeric fields.
+		total, err := strconv.ParseInt(m[2], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("line %d: invalid total size: %v", i+1, err)
+		}
+		comp, err := strconv.ParseInt(m[3], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("line %d: invalid compressed size: %v", i+1, err)
+		}
+
+		// If the name is "All archives", this is a summary stats block.
+		// If the name is "(unique data)", this is a continuation block.
+		// Otherwise, this is an archive-specific block.
+		switch tag := m[1]; tag {
+		case "All archives":
+			cur = &Sizes{
+				InputBytes:      total,
+				CompressedBytes: comp,
+			}
+			info.All = cur
+
+		case "(unique data)":
+			if cur == nil {
+				return nil, fmt.Errorf("line %d: unexpected continuation line", i+1)
+			}
+			cur.UniqueBytes = total
+			cur.CompressedUniqueBytes = comp
+			cur = nil
+
+		default:
+			cur = &Sizes{InputBytes: total, CompressedBytes: comp}
+			info.Archive[tag] = cur
+		}
+	}
+	return info, nil
 }
 
 func (c *Config) base(rest ...string) (string, []string) {
